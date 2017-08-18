45726742650793816237a2912a67eba4bb17f14e
TWSICommunity <gamewolf10@gmail.com>
2017-08-18 13:20:31 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+ <head>
+  <title>TWSICommunity - Raspberry pi zero w webserver</title>
+ </head>
+ <body style="background-color: rgb(0, 0, 91);">
+  <div style="background-color: gray; border:1px solid black; margin:100px 100px 100px 100px;"><center><h1>Welcome users!</h1>
+  <h4>Server running rasbian and the latest version</h4>
+  <a href="wallcomment/index.php">Wall commenting page</a></center>
+  <!-- <p align="center" style='border: 1px solid black; background-color: gray; width: 50px;'>Wip</p> -->
+  <p style="border: 1px solid black; background-color: rgb(0,0,150); color: white; font-size: 28px; margin-left: 100px; margin-right: 100px;">Welcome to my new server hosted by the Raspberry Pi Zero W<br>
+  <br>
+  This is where TWSICommunity.com be once everything is transferred onto the pi. Which means this device will last longer since it's powered by a battery power bank. Plus it lasts longer and power doesn't shut off on the pi since it's on battery powered. So all data that have been stored will be kept safe during power outages.<br>
+  <br>
+  So in this case everything wouldn't be on an external hard drive all the time. Isn't portable great :D.</p><p align="center"><b>TWSICommunity - Copyright 2017 - 2018</b></p></div>
+ </body>
+</html>
